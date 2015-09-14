c9dd6fcdb44ee1fc5c70e7930fdc96017c14b929
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-09-14 09:05:47 -0400

    Removes artifacts for demo.

--- src/main/scala/org/example/materializer/AkkaCapability.scala
+++
@@ -1,9 +0,0 @@
-package org.example.materializer
-
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-
-trait AkkaCapability {
-  implicit val actorSystem = ActorSystem("songs")
-  implicit val materializer = ActorMaterializer()
-}
